089f7af6a0fb0c3a1f94988a2334ffad8fd75deb
welfarecode <20095@galmae.hs.kr>
2021-07-14 13:27:30 +0900

    welfarecode

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
